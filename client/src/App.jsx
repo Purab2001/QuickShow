f6e84a97286cf3fac3691c3c10e2da6f0a1e2c46
Abir Shahadat Purab <a.s.purab0@gmail.com>
2025-07-07 12:12:08 +0600

    feat: add admin panel components and routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,11 @@ import MovieDetails from './pages/MovieDetails'
 import SeatLayout from './pages/SeatLayout'
 import MyBookings from './pages/MyBookings'
 import Favorite from './pages/Favorite'
+import Layout from './pages/Admin/Layout'
+import Dashboard from './pages/Admin/Dashboard'
+import AddShows from './pages/Admin/AddShows'
+import ListShows from './pages/Admin/ListShows'
+import ListBookings from './pages/Admin/ListBookings'
 
 const App = () => {
 
@@ -23,7 +28,13 @@ const App = () => {
         <Route path='/movies/:id' element={<MovieDetails/>} />
         <Route path='/movies/:id/:date' element={<SeatLayout/>} />
         <Route path='/my-bookings' element={<MyBookings/>} />
-        <Route path='/favorite' element={<Favorite/>} />
+        <Route path='/favorite' element={<Favorite />} />
+        <Route path='/admin/*' element={<Layout/>}>
+          <Route index element={<Dashboard/>} />
+          <Route path='add-shows' element={<AddShows/>} />
+          <Route path='list-shows' element={<ListShows/>} />
+          <Route path='list-bookings' element={<ListBookings/>} />
+        </Route>
       </Routes>
       {!isAdminRoute && <Footer />}
     </>
