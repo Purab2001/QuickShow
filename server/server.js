ca0b884fcc7ab20c2b6d09eeac5e7e102bfb6894
Abir Shahadat Purab <a.s.purab0@gmail.com>
2025-07-19 23:26:53 +0600

    feat: implement booking and show management features with API routes and controllers

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ import connectDB from './configs/db.js';
 import { clerkMiddleware } from "@clerk/express";
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js";
+import showRouter from './routes/showRoutes.js';
+import bookingRouter from './routes/bookingRoutes.js';
 
 const app = express();
 const PORT = 3000;
@@ -19,6 +21,8 @@ app.use(clerkMiddleware());
 // Routes
 app.get('/', (req, res) => {res.send('Server is Live!')});
 app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use('/api/show', showRouter);
+app.use('/api/booking', bookingRouter);
 
 // Start the server
 app.listen(PORT, () => {
